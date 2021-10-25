52c9d23713c156323e7fd78e8eed90de438c8ad6
Benjamin Alterauge <5332429+ageeye@users.noreply.github.com>
2021-10-25 20:32:04 +0200

    Fix extractXML and use now xml praser

--- src/e57importer.py
+++ src/e57importer.py
@@ -34,8 +34,6 @@
 import numpy as np
 
 E57_PAGE_CRC     = 4
-E57_PRE_OFFSET   = 256 
-
 
 class E57:
 
@@ -43,6 +41,7 @@ class E57:
         self.filename  = filename
         self.checkfile = checkfile 
         self.readHeader()
+        self.buildRoot()
         
     def readHeader(self):
         dtheader = np.dtype( [ ('fileSignature', np.dtype('S8')),
@@ -60,7 +59,7 @@ class E57:
         self.xmlPhysicalOffset  = header['xmlPhysicalOffset'][0]
         self.xmlLogicalLength   = header['xmlLogicalLength'][0]
         self.pageSize           = header['pageSize'][0]
-        self.pageContent        = int(self.pageSize - E57_PAGE_CRC)
+        self.pageContent        = (self.pageSize - E57_PAGE_CRC).astype(np.uint64)
         
         if self.checkfile:
             # check file format
@@ -73,38 +72,62 @@ class E57:
     def extractXML(self):
         # caculate the pages
         # one page include the content and crc cecksum
-        start  = self.xmlPhysicalOffset % self.pageSize
-        first  = np.array((start-E57_PAGE_CRC+E57_PRE_OFFSET).astype(np.uint64))
-        diff   = (self.xmlLogicalLength - start).astype(np.uint64)
+        start  = (self.xmlPhysicalOffset % self.pageSize)
+        first  = np.array((self.pageContent-start).astype(np.uint64))
+        diff   = (self.xmlLogicalLength - first).astype(np.uint64)
         pages  = np.full((diff // self.pageSize), self.pageContent, np.uint64)
         modulo = (diff % self.pageSize).astype(np.uint64) 
         pages = np.append(first, pages)
-        if (modulo > 0):
-            pages = np.append(pages,  np.array((modulo-E57_PAGE_CRC).astype(np.uint64))) 
+        pages = np.append(pages,  np.array((modulo).astype(np.uint64))) 
         offset = self.xmlPhysicalOffset
-        
+        pages[-1] = (pages[-1] + (len(pages)-2)*E57_PAGE_CRC).astype(np.uint64) 
+        while (pages[-1] >= self.pageContent):
+            pages = np.append(pages,(pages[-1]-self.pageContent+E57_PAGE_CRC).astype(np.uint64))
+            pages[-2] = self.pageContent
+                
         # get xml content
         xmltxt = bytearray()
         for page in pages:
             xmltxt.extend(np.fromfile(self.filename, np.byte, count=page, offset=offset))
             offset = np.sum([offset, page, E57_PAGE_CRC],dtype=np.uint64)
         
-        return xmltxt.decode('utf-8') # why only this work?, it should be utf-8
+        #print(xmltxt)
+        return xmltxt.decode('utf-8')
         
+        
+    def buildRoot(self):
+        import xml.etree.ElementTree as ET
+        xmltxt = self.extractXML()
+        self.root = ET.fromstring(xmltxt) 
+        
+    def getNS(self):
+        return {'e57':'http://www.astm.org/COMMIT/E57/2010-e57-v1.0'}
+        
+    def findElement(self, name, parent=None):
+        if (parent is None):
+            parent = self.root
+        return parent.find('e57:'+name, self.getNS())
+        
+    def iterElements(self, name, parent=None):
+        if (parent is None):
+            parent = self.root
+        return parent.iterfind('.//e57:'+name, self.getNS())
         
     def extractCompressedVector(self):
-        import re, struct
+        data = self.findElement('data3D')
+        for pts in self.iterElements('points'):
+            if (pts.attrib['type']=='CompressedVector'):
+                pos = pts.attrib['fileOffset']
+                cnt = pts.attrib['recordCount']
+                
+                proto = self.findElement('prototype', pts)
+                cx = self.findElement('cartesianX', proto)
+                xmin = cx.attrib['minimum']
+                
         
-        txt = self.extractXML()
-        vec = re.search(r'CompressedVector(.*?)>',txt).group(1)
-        pos =  int(re.search(r'fileOffset="(.*?)"',vec).group(1))
-        cnt =  int(re.search(r'recordCount="(.*?)"',vec).group(1))
-        
-        offsetx = (pos * self.pageSize).astype(np.uint64)
-        
-        print(offsetx)
-        print(pos)
-        print(cnt)
+                print(pos)
+                print(cnt)
+                print(xmin)
 
 # testing   
 # test data
@@ -113,6 +136,9 @@ class E57:
 from pathlib import Path
     
 e57 = E57(str(Path.home())+'/Downloads/bunnyDouble.e57')
-txt = e57.extractXML()
-print(txt)
+#e57 = E57(str(Path.home())+'/Downloads/pump.e57')
+#e57 = E57(str(Path.home())+'/Downloads/garage.e57')
+
+e57.extractCompressedVector()
+
 
